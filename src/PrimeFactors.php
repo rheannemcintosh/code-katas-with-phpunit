c29563544571c2990401f5d2239ba72d34f6d7e5
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-05-19 23:36:59 +0100

    Create the basic PrimeFactors class

--- src/PrimeFactors.php
+++ src/PrimeFactors.php
@@ -6,5 +6,21 @@ namespace App;
 
 class PrimeFactors
 {
+    public function generate($number)
+    {
+        $factors = [];
+        $divisor = 2;
 
+        while ($number > 1) {
+            while ($number % $divisor == 0) {
+                $factors[] = $divisor;
+
+                $number = $number / $divisor;
+            }
+
+            $divisor++;
+        }
+
+        return $factors;
+    }
 }
