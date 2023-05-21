3157df9b4c5502e8087b5b8f45c83767f1f66658
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-05-21 18:14:28 +0100

    Create a basic function which passes for 1 & 2 roman numerals

--- src/RomanNumerals.php
+++ src/RomanNumerals.php
@@ -6,5 +6,12 @@ namespace App;
 
 class RomanNumerals
 {
-
+    public static function generate($number)
+    {
+        if ($number > 1) {
+            return 'II';
+        }
+        
+        return 'I';
+    }
 }
