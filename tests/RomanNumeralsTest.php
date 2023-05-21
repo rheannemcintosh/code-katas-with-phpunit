9f749a3bfe63bbe66143037628f8ad21fd7d0dbd
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-05-21 18:53:28 +0100

    Make lookup table and tests for all numbers up to 3999

--- tests/RomanNumeralsTest.php
+++ tests/RomanNumeralsTest.php
@@ -22,6 +22,21 @@ class RomanNumeralsTest extends TestCase
             [2, 'II'],
             [3, 'III'],
             [4, 'IV'],
+            [5, 'V'],
+            [6, 'VI'],
+            [7, 'VII'],
+            [8, 'VIII'],
+            [9, 'IX'],
+            [10, 'X'],
+            [40, 'XL'],
+            [50, 'L'],
+            [90, 'XC'],
+            [100, 'C'],
+            [400, 'CD'],
+            [500, 'D'],
+            [900, 'CM'],
+            [1000, 'M'],
+            [3999, 'MMMCMXCIX'],
         ];
     }
 }
