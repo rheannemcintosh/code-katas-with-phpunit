187d451fd48c041cb0eeaf00b5d16bcdc5049ea5
Rheanne McIntosh <rheanne.mcintosh@visualsoft.co.uk>
2023-05-21 18:14:41 +0100

    Create two tests for roman numerals 1 & 2

--- tests/RomanNumeralsTest.php
+++ tests/RomanNumeralsTest.php
@@ -1,9 +1,20 @@
 <?php
 
 
+use App\RomanNumerals;
 use PHPUnit\Framework\TestCase;
 
 class RomanNumeralsTest extends TestCase
 {
+    /** @test */
+    function it_generates_the_roman_numeral_for_1()
+    {
+        $this->assertEquals('I', RomanNumerals::generate(1));
+    }
 
+    /** @test */
+    function it_generates_the_roman_numeral_for_2()
+    {
+        $this->assertEquals('II', RomanNumerals::generate(1));
+    }
 }
