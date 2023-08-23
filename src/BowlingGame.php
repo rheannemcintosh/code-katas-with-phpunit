fcc0e4c92003d7f23a66b72afde6ec874299d48c
rheannemcintosh <rheannemcintosh@gmail.com>
2023-08-23 16:07:42 +0100

    Create test for giving a bonus if a spare has been achieved

--- src/BowlingGame.php
+++ src/BowlingGame.php
@@ -4,6 +4,7 @@ namespace App;
 
 class BowlingGame
 {
+    const FRAMES_PER_GAME = 10;
     protected array $rolls = [];
 
     public function roll(int $pins)
@@ -13,6 +14,24 @@ class BowlingGame
 
     public function score()
     {
-        return array_sum($this->rolls);
+        $score = 0;
+        $roll = 0;
+
+        foreach (range(1, self::FRAMES_PER_GAME) as $frame) {
+            // Check for a Spare
+            if($this->rolls[$roll] + $this->rolls[$roll + 1] === 10) {
+                // You got a spare
+                $score += $this->rolls[$roll] + $this->rolls[$roll + 1];
+                $score += $this->rolls[$roll + 2];
+
+                    $roll += 2;
+            } else {
+                $score += $this->rolls[$roll] + $this->rolls[$roll + 1];
+
+                $roll += 2;
+            }
+        }
+
+        return $score;
     }
 }
--- tests/BowlingGameTest.php
+++ tests/BowlingGameTest.php
@@ -28,4 +28,21 @@ class BowlingGameTest extends TestCase
 
         $this->assertSame(20, $game->score());
     }
+
+    /** @test */
+    function it_awards_a_one_roll_bonus_for_every_spare()
+    {
+        $game = new BowlingGame();
+
+        $game->roll(5);
+        $game->roll(5); // spare
+
+        $game->roll(8);
+
+        foreach(range(1, 17) as $roll) {
+            $game->roll(0);
+        }
+
+        $this->assertSame(26, $game->score());
+    }
 }
