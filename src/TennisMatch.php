878ad8b15b6523946d9ee73fd56dcc7aab30cfb0
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-20 21:56:04 +0100

    Refactor into a hasWinner method

--- src/TennisMatch.php
+++ src/TennisMatch.php
@@ -9,12 +9,8 @@ class TennisMatch
 
     public function score()
     {
-        if($this->playerOnePoints > 3 && $this->playerOnePoints >= $this->playerTwoPoints +2) {
-            return 'Winner: Player 1';
-        }
-
-        if($this->playerTwoPoints > 3 && $this->playerTwoPoints >= $this->playerOnePoints +2) {
-            return 'Winner: Player 2';
+        if ($this->hasWinner()) {
+            return $this->playerOnePoints > $this->playerTwoPoints ? 'Winner: Player 1' : 'Winner: Player 2';
         }
 
         return sprintf(
@@ -32,6 +28,19 @@ class TennisMatch
     public function pointToPlayerTwo()
     {
         $this->playerTwoPoints++;
+    }
+
+    public function hasWinner()
+    {
+        if($this->playerOnePoints > 3 && $this->playerOnePoints >= $this->playerTwoPoints +2) {
+            return true;
+        }
+
+        if($this->playerTwoPoints > 3 && $this->playerTwoPoints >= $this->playerOnePoints +2) {
+            return true;
+        }
+
+        return false;
     }
 
     protected function pointsToScore($points)
