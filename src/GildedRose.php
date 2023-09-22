f6dd5237f83af90e63def40edd227bedca00c605
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-22 14:31:39 +0100

    Update class to handle aged brie

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -28,6 +28,10 @@ class GildedRose
             return $this->normalTick();
         }
 
+        if ($this->name === 'Aged Brie') {
+            return $this->brieTick();
+        }
+
         if ($this->name != 'Aged Brie' and $this->name != 'Backstage passes to a TAFKAL80ETC concert') {
             if ($this->quality > 0) {
                 if ($this->name != 'Sulfuras, Hand of Ragnaros') {
@@ -87,6 +91,21 @@ class GildedRose
 
         if ($this->quality < 0) {
             $this->quality = 0;
+        }
+    }
+
+    private function brieTick()
+    {
+        $this->sellIn -= 1;
+
+        if ($this->quality < 50) {
+            $this->quality += 1;
+        }
+
+        if ($this->quality < 50) {
+            if ($this->sellIn <= 0) {
+                $this->quality += 1;
+            }
         }
     }
 }
