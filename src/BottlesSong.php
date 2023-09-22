10aa8c407a00e8c5d0aa69c2e03c63cf0e285b7c
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-22 16:40:57 +0100

    Create the verse method where the number of bottles is between 99 and 3

--- src/BottlesSong.php
+++ src/BottlesSong.php
@@ -5,4 +5,12 @@ namespace App;
 class BottlesSong
 {
 
+    public function verse($number)
+    {
+        return
+            "$number bottles of beer on the wall\n" .
+            "$number bottles of beer\n" .
+            "Take one down and pass it around\n" .
+            ($number - 1) . " bottles of beer on the wall\n";
+    }
 }
