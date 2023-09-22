7254d47ea7b9f007faa46917da7587c31ab9e414
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-22 16:44:08 +0100

    Create the verse method where the number of bottles is 2

--- src/BottlesSong.php
+++ src/BottlesSong.php
@@ -11,6 +11,6 @@ class BottlesSong
             "$number bottles of beer on the wall\n" .
             "$number bottles of beer\n" .
             "Take one down and pass it around\n" .
-            ($number - 1) . " bottles of beer on the wall\n";
+            ($number - 1) . ($number === 2 ? " bottle" : " bottles") . " of beer on the wall\n";
     }
 }
