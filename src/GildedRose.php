baa7e8cd5cc092278da998aa23ce452c17b44565
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-22 14:33:26 +0100

    Refactor brieTick()

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -97,15 +97,14 @@ class GildedRose
     private function brieTick()
     {
         $this->sellIn -= 1;
+        $this->quality += 1;
 
-        if ($this->quality < 50) {
+        if ($this->sellIn <= 0) {
             $this->quality += 1;
         }
 
-        if ($this->quality < 50) {
-            if ($this->sellIn <= 0) {
-                $this->quality += 1;
-            }
+        if ($this->quality > 50) {
+            $this->quality = 50;
         }
     }
 }
