786b23784ee9e9817ee6645faa06234052712a2e
rheannemcintosh <rheannemcintosh@gmail.com>
2023-09-22 15:32:30 +0100

    Create a new conjured class

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -8,7 +8,8 @@ class GildedRose
         'normal' => Item::class,
         'Aged Brie' => Brie::class,
         'Sulfuras, Hand of Ragnaros' => Sulfuras::class,
-        'Backstage passes to a TAFKAL80ETC concert' => BackstagePass::class
+        'Backstage passes to a TAFKAL80ETC concert' => BackstagePass::class,
+        'Conjured Mana Cake' => Conjured::class,
     ];
 
     public static function of($name, $quality, $sellIn)
